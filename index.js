8a6a3f256c81cfb68071ad70365b8dd451d7ee0b
Swarup Karmokar <swarupkarmokar123@gmail.com>
2023-06-19 17:56:03 +0530

    App structure created

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+require('dotenv').config();
+
+// CREATING PORT FOR APP 
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// MIDDLEWARE 
+app.use(express.json());
+
+
+// ADDING TO THE PORT 
+app.listen(port, (err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    console.log(`port running at: http://localhost:${port}`)
+})
